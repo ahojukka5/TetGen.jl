dce7ae71440ad66ba8b0c8f01cca9d35a023321d
Tero Frondelius <tero.frondelius@gmail.com>
2015-06-28 16:33:55 +0300

    build.jl works partly

--- deps/build.jl
+++ deps/build.jl
@@ -2,23 +2,22 @@ using BinDeps
 
 @BinDeps.setup
 
-tetgen = library_dependency("libtet")
+libtet = library_dependency("libtet", aliases = ["libtet.so"])
+sources = provides(Sources,URI("http://tetgen.org/files/tetgen1.4.3.tar.gz"),libtet,unpacked_dir="tetgen1.4.3")
+builddir = joinpath(BinDeps.builddir(libtet),libtet.name)
+srcdir = joinpath(BinDeps.srcdir(libtet),"tetgen1.4.3")
 
-sources = provides(Sources,URI("http://tetgen.org/files/tetgen1.4.3.tar.gz"),tetgen,unpacked_dir="tetgen1.4.3")
-builddir = joinpath(BinDeps.builddir(tetgen),tetgen.name)
-bindir = joinpath(builddir,"bin")
-steps = BinDeps.generate_steps(tetgen, Autotools(configure_options=["--enable-shared"]),Dict())
-options = ["--enable-shared","--enable-debug"]
+provides(SimpleBuild,
+         (@build_steps begin
+            GetSources(libtet)
+            CreateDirectory(builddir)
+            @build_steps begin
+              ChangeDirectory(builddir)
+              `g++ -fPIC -O0 -c $srcdir/predicates.cxx`
+              `g++ -fPIC -DTETLIBRARY -c $srcdir/tetgen.cxx`
+              `g++ -shared -fPIC tetgen.o predicates.o -o libtet.so`
+            end
+          end),
+         libtet, os = :Unix)
 
-provides(BuildProcess,(@build_steps begin
-         GetSources(tetgen)
-         CreateDirectory(builddir)
-#         ChangeDirectory(builddir)
-#         # Not an actual configure
-#         MakeTargets(["tetlib"])
-#         #`chmod +x $(BinDeps.bindir(pythia))/pythia8-config`
-         end),tetgen)
-
-# #provides(BuildProcess,Autotools(libtarget = "src/.libs/libfastjet.la"),fastjet)
-
-# @BinDeps.install Dict( :tetgen => :libtetgen)
+@BinDeps.install Dict( :libtet => :libtet)
