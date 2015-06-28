047c234921c54ca8bf025c2f9d4a78a2d332b785
Tero Frondelius <tero.frondelius@gmail.com>
2015-06-28 17:50:18 +0300

    starting the coding

--- src/TetGen.jl
+++ src/TetGen.jl
@@ -1,5 +1,16 @@
 module TetGen
+using Cxx
 
-# package code goes here
+cxx"""#define TETLIBRARY"""
+const header_dir = joinpath(Pkg.dir("TetGen"),"deps/usr/include/")
+const tetgen_so = joinpath(Pkg.dir("TetGen"),"deps/usr/lib/libtet.so")
+addHeaderDir(header_dir, kind=C_System)
+Libdl.dlopen(tetgen_so, Libdl.RTLD_GLOBAL)
+cxxinclude("tetgen.h")
+
+in_ = @cxxnew tetgenio()
+out_ = @cxxnew tetgenio()
+addin_ = @cxxnew tetgenio()
+bgmin_ = @cxxnew tetgenio()
 
 end # module
